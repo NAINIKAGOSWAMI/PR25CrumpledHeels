3bd3dd0465c18191d07fbe53dedbc7a627ba4d74
NAINIKAGOSWAMI <74713715+NAINIKAGOSWAMI@users.noreply.github.com>
2021-02-16 19:27:33 +0530

    Add files via upload

--- Dustbin.js
+++ Dustbin.js
@@ -2,11 +2,13 @@ class Dustbin
 {
 	constructor(x,y)
 	{
+		this.image = loadImage("dustbingreen.png");
 		this.x=x;
 		this.y=y;
 		this.dustbinWidth=200;
 		this.dustbinHeight=213;
 		this.wallThickness=20;
+		
 		
 		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
 		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
